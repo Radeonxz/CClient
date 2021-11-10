eb3c7ca58d46c2e2a8dda79d11252267f2325771
RadeonXz <xuanxxzhao@gmail.com>
2021-11-09 21:58:11 -0500

    chore: add AsyncFetchData

--- CClient/Server.cpp
+++ CClient/Server.cpp
@@ -1,4 +1,4 @@
-
+#include <chrono>
 #include <iostream>
 
 #ifdef _WIN32
@@ -9,6 +9,26 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+std::vector<char> vBuffer(20 * 1024);
+
+void AsyncFetchData(asio::ip::tcp::socket& socket)
+{
+	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+		[&](std::error_code ec, std::size_t length)
+		{
+			if (!ec)
+			{
+				std::cout << "\n\nRead " << length << " bytes\n\n";
+
+				for (int i = 0; i < length; i++)
+					std::cout << vBuffer[i];
+
+				AsyncFetchData(socket);
+			}
+		}
+	);
+}
+
 int main()
 {
 	asio::error_code ec;
@@ -16,8 +36,16 @@ int main()
 	// Create the asio context
 	asio::io_context context;
 
+	//  Tasks for asio context when context doesn't finish
+	asio::io_context::work idleWork(context);
+
+	// Spawn context in threads
+	std::thread thrContext = std::thread([&]() { context.run(); });
+
 	// Get the address of the connection
-	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+	std::string shortUri = "93.184.216.34";
+	std::string longUri = "51.38.81.49";
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address(longUri, ec), 80);
 
 	// Create a socket, associate the context
 	asio::ip::tcp::socket socket(context);
@@ -32,6 +60,37 @@ int main()
 	else
 	{
 		std::cout << "Failed to connect to address:\n" << ec.message() << std::endl;
+	}
+
+	if (socket.is_open())
+	{
+		AsyncFetchData(socket);
+
+		std::string sRequest =
+			"GET /index.html HTTP/1.1\r\n"
+			"Host: uri.com\r\n"
+			"Connection: close\r\n\r\n";
+
+		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+		// Wait until recive response from the server then write
+		/*socket.wait(socket.wait_read);
+
+		size_t bytes = socket.available();
+		std::cout << "Bytes Available: " << bytes << std::endl;
+
+		if (bytes > 0)
+		{
+			std::vector<char> vBuffer(bytes);
+			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+
+			for (auto c : vBuffer)
+				std::cout << c;
+		}*/
+
+		// Wait for 20000ms in the background to wait for data transfer
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(20000ms);
 	}
 
 	system("pause");
