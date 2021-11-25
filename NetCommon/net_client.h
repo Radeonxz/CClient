b1f8d57ab1fcb9bf4cf1e86d45803a7ddb5efe05
RadeonXz <xuanxxzhao@gmail.com>
2021-11-24 22:09:42 -0500

    chore: add server

--- NetCommon/net_client.h
+++ NetCommon/net_client.h
@@ -12,7 +12,61 @@ namespace olc
 		template <typename T>
 		class client_interface
 		{
+			client_interface() : m_socket(m_context)
+			{
+				// init the socket with the io context
+			}
 
+			virtual ~client_interface()
+			{
+				// if client is destroyed then disconnect from server
+				Disconnect();
+			}
+
+		public:
+			// connect to server with hostname/ip-address and port
+			bool Connect(const std::string& host, const unit16_t port)
+			{
+
+				return false;
+			}
+
+			// disconnect from server
+			void Disconnect()
+			{
+
+			}
+
+			// check if client is connected to the server
+			bool IsConnected()
+			{
+				return false;
+			}
+
+			// retrieve queue of messages from server
+			tsqueue<owned_message<T>>& Incoming()
+			{
+				return m_qMessagesIn;
+			}
+
+		protected:
+			// asio context handles data tansfer
+			asio::io_context m_context;
+
+			// spawn a thread to excute its work commands
+			std::thread thrContext;
+
+			// hardware socket that is connected to the server
+			asio::ip::tcp::socket m_socket;
+
+			// Client has a single instance of the connection obj for data transfer
+			std::unique_ptr<connection<T>> m_connection;
+		
+		private:
+			// thread safe queue of incoming messages from server
+			tsqueue<owned_message<T>> m_qMessagesIn;
 		};
+
+
 	}
 }
