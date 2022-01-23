20a66c816764625417735546f8f86445086d87ef
RadeonXz <xuanxxzhao@gmail.com>
2022-01-23 15:45:19 -0500

    chore: server and client

--- NetClient/SimpleClient.cpp
+++ NetClient/SimpleClient.cpp
@@ -1,51 +1,162 @@
+//#include <iostream>
+//#include <olc_net.h>
+//
+//enum class CustomMsgTypes : uint32_t
+//{
+//	FireBullet,
+//	MovePlayer
+//};
+//
+//class CustomClient : public olc::net::client_interface<CustomMsgTypes>
+//{
+//	public:
+//		bool FireBullet(float x, float y)
+//		{
+//			olc::net::message<CustomMsgTypes> msg;
+//			msg.header.id = CustomMsgTypes::FireBullets;
+//			msg << x << y;
+//			Sennd(msg);
+//		}
+//};
+//
+//int main()
+//{
+//	/*olc::net::message<CustomMsgTypes> msg;
+//	msg.header.id = CustomMsgTypes::FireBullet;
+//
+//	int a = 1;
+//	bool b = true;
+//	float c = 3.14159f;
+//
+//	struct
+//	{
+//		float x;
+//		float y;
+//	} d[5];
+//
+//	msg << a << b << c << d;
+//
+//	a = 99;
+//	b = false;
+//	c = 99.0f;
+//
+//	msg >> d >> c >> b >> a;
+//
+//	return 0;*/
+//
+//	CustomClient c;
+//	c.Connect("", 60000);
+//	c.FireBullet(2.0f, 5.0f);
+//	return 0
+//}
+
 #include <iostream>
 #include <olc_net.h>
 
 enum class CustomMsgTypes : uint32_t
 {
-	FireBullet,
-	MovePlayer
+	ServerAccept,
+	ServerDeny,
+	ServerPing,
+	MessageAll,
+	ServerMessage,
 };
+
+
 
 class CustomClient : public olc::net::client_interface<CustomMsgTypes>
 {
-	public:
-		bool FireBullet(float x, float y)
-		{
-			olc::net::message<CustomMsgTypes> msg;
-			msg.header.id = CustomMsgTypes::FireBullets;
-			msg << x << y;
-			Sennd(msg);
-		}
+public:
+	void PingServer()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ServerPing;
+
+		// Caution with this...
+		std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+
+		msg << timeNow;
+		Send(msg);
+	}
+
+	void MessageAll()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::MessageAll;
+		Send(msg);
+	}
 };
 
 int main()
 {
-	/*olc::net::message<CustomMsgTypes> msg;
-	msg.header.id = CustomMsgTypes::FireBullet;
-
-	int a = 1;
-	bool b = true;
-	float c = 3.14159f;
-
-	struct
-	{
-		float x;
-		float y;
-	} d[5];
-
-	msg << a << b << c << d;
-
-	a = 99;
-	b = false;
-	c = 99.0f;
-
-	msg >> d >> c >> b >> a;
-
-	return 0;*/
-
 	CustomClient c;
-	c.Connect("", 60000);
-	c.FireBullet(2.0f, 5.0f);
-	return 0
+	c.Connect("127.0.0.1", 60000);
+
+	bool key[3] = { false, false, false };
+	bool old_key[3] = { false, false, false };
+
+	bool bQuit = false;
+	while (!bQuit)
+	{
+		if (GetForegroundWindow() == GetConsoleWindow())
+		{
+			key[0] = GetAsyncKeyState('1') & 0x8000;
+			key[1] = GetAsyncKeyState('2') & 0x8000;
+			key[2] = GetAsyncKeyState('3') & 0x8000;
+		}
+
+		if (key[0] && !old_key[0]) c.PingServer();
+		if (key[1] && !old_key[1]) c.MessageAll();
+		if (key[2] && !old_key[2]) bQuit = true;
+
+		for (int i = 0; i < 3; i++) old_key[i] = key[i];
+
+		if (c.IsConnected())
+		{
+			if (!c.Incoming().empty())
+			{
+
+
+				auto msg = c.Incoming().pop_front().msg;
+
+				switch (msg.header.id)
+				{
+				case CustomMsgTypes::ServerAccept:
+				{
+					// Server has responded to a ping request				
+					std::cout << "Server Accepted Connection\n";
+				}
+				break;
+
+
+				case CustomMsgTypes::ServerPing:
+				{
+					// Server has responded to a ping request
+					std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+					std::chrono::system_clock::time_point timeThen;
+					msg >> timeThen;
+					std::cout << "Ping: " << std::chrono::duration<double>(timeNow - timeThen).count() << "\n";
+				}
+				break;
+
+				case CustomMsgTypes::ServerMessage:
+				{
+					// Server has responded to a ping request	
+					uint32_t clientID;
+					msg >> clientID;
+					std::cout << "Hello from [" << clientID << "]\n";
+				}
+				break;
+				}
+			}
+		}
+		else
+		{
+			std::cout << "Server Down\n";
+			bQuit = true;
+		}
+
+	}
+
+	return 0;
 }
